3c1537c2303ac1bbd48513408463da757b1566a4
ingmarschuster <ingmar.schuster.linguistics@gmail.com>
2013-08-06 13:32:26 +0200

    Added Jensen-Shannon divergence. Not sure what would be the preferred order for function arguments and preferred file names. If I recall correctly, this package stands to be integrated into other stats packages, so I leave this up to John.

--- src/KLDivergence.jl
+++ src/KLDivergence.jl
@@ -1,5 +1,5 @@
 module KLDivergence
-	export kl_divergence
+	export kl_divergence, kl_divergence_sample_prob, js_divergence, js_divergence_sample_prob, divergence
 
 	include("kl.jl")
 end
--- src/kl.jl
+++ src/kl.jl
@@ -1,15 +1,29 @@
 using Distributions
 
-# Use Monte Carlo methods to estimate the KL-Divergence between two
+# Use Monte Carlo methods to estimate the Kullback-Leibler-Divergence or
+# Jensen-Shannon-Divergence between two
 # theoretical distributions. Take random samples of source distribution.
 # Estimate cost at those points and weight by probability.
-function kl_divergence(d1::Distribution, d2::Distribution, samples::Integer)
+
+function kl_divergence_sample_prob(p_d1,p_d2)
+    return -p_d1 * log(p_d2 / p_d1)
+end
+
+function js_divergence_sample_prob(p_d1, p_d2)
+    p_m = (p_d1 + p_d2) / 2
+    return (kl.kl_divergence_single(p_d1, p_m) + kl.kl_divergence_single(p_d2, p_m)) / 2
+end
+
+function divergence(d1::Distribution, d2::Distribution, samples::Integer, sample_divergence_func)
     divergence = 0.0
     for i in 1:samples
         x = rand(d1)
-        divergence += -pdf(d1, x) * log(pdf(d2, x) / pdf(d1, x))
+        divergence += sample_divergence_func(pdf(d1, x), pdf(d2, x))
     end
     return divergence
 end
 
-kl_divergence(d1::Distribution, d2::Distribution) = kl_divergence(d1, d2, 10_000)
+kl_divergence(d1::Distribution, d2::Distribution) = kl_divergence(d1, d2, 10_000, kl_divergence_sample_prob)
+js_divergence(d1::Distribution, d2::Distribution) = kl_divergence(d1, d2, 10_000, js_divergence_sample_prob)
+
+
