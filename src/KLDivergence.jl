99692c261e8750a7b21112f1ae374934c975a0e4
ingmarschuster <ingmar.schuster.linguistics@gmail.com>
2013-08-07 11:35:00 +0200

    Added Jensen-Shannon divergence and symmetrized Kullback-Leibler. Changed functi Changed test code to also test new divergence functions.

--- src/KLDivergence.jl
+++ src/KLDivergence.jl
@@ -1,5 +1,5 @@
 module KLDivergence
-	export kl_divergence
+	export kl_divergence, kl_sample_divergence, skl_divergence, skl_sample_divergence, js_divergence, js_sample_divergence, mc_divergence_estimate
 
 	include("kl.jl")
 end
--- src/kl.jl
+++ src/kl.jl
@@ -1,15 +1,40 @@
 using Distributions
 
-# Use Monte Carlo methods to estimate the KL-Divergence between two
+# non-symmetric (i.e. conventional) divergence for a single sample
+
+function kl_sample_divergence(dist1_probab,dist2_probab)
+    return -dist1_probab * log(dist2_probab / dist1_probab)
+end
+
+# symmetrized divergence for a single sample
+
+function skl_sample_divergence(dist1_probab,dist2_probab)
+    return kl_sample_divergence(dist1_probab,dist2_probab) + kl_sample_divergence(dist2_probab,dist2_probab)
+end
+
+# Jensen-Shannon divergence for a single sample (symmetric)
+
+function js_sample_divergence(dist1_probab, dist2_probab)
+    const mean_probab = (dist1_probab + dist2_probab) / 2
+    return (kl_sample_divergence(dist1_probab, mean_probab) + kl_sample_divergence(dist2_probab, mean_probab)) / 2
+end
+
+
+# Use Monte Carlo methods to estimate the divergences between two
 # theoretical distributions. Take random samples of source distribution.
 # Estimate cost at those points and weight by probability.
-function kl_divergence(d1::Distribution, d2::Distribution, samples::Integer)
+
+function mc_divergence_estimate(d1::Distribution, d2::Distribution, samples::Integer, sample_divergence_func)
     divergence = 0.0
     for i in 1:samples
         x = rand(d1)
-        divergence += -pdf(d1, x) * log(pdf(d2, x) / pdf(d1, x))
+        divergence += sample_divergence_func(pdf(d1, x), pdf(d2, x))
     end
     return divergence
 end
 
-kl_divergence(d1::Distribution, d2::Distribution) = kl_divergence(d1, d2, 10_000)
+kl_divergence(d1::Distribution, d2::Distribution) = mc_divergence_estimate(d1, d2, 10_000, kl_sample_divergence)
+skl_divergence(d1::Distribution, d2::Distribution) = mc_divergence_estimate(d1, d2, 10_000, skl_sample_divergence)
+js_divergence(d1::Distribution, d2::Distribution) = mc_divergence_estimate(d1, d2, 10_000, js_sample_divergence)
+
+
